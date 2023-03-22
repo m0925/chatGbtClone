99775e060ddcd40ddf5931a96919f5f6732fca93
m0925 <esmigmic@gmail.com>
2023-03-22 16:00:59 +0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ loader(messageDiv);
 
  // fetch data from server -> bot's response
 
- const response = await fetch('http://localhost:5000', {
+ const response = await fetch('https://chatgbtclone.onrender.com', {
   method: 'POST' ,
   headers:{
     'content-type': 'application/json'
