01130d1f7d1bef48d77aaebabb83027fc9684db7
m0925 <esmigmic@gmail.com>
2023-03-23 13:42:49 +0300

    update the server url

--- server/server.js
+++ server/server.js
@@ -49,4 +49,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('server is running on port https://chatgbtclone.onrender.com'));
